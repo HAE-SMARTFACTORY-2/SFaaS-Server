90dad5d0f65f65ac6d0bdba6f463669c2397bfc4
Choi YoungLyn <joeuni0129@gmail.com>
2024-12-13 22:47:34 +0900

    [Fix] #7 - Response DTO 변경

--- sfaas/src/main/java/org/hae/sfaas/domain/user/dto/response/UserDetailInfoResponse.java
+++ sfaas/src/main/java/org/hae/sfaas/domain/user/dto/response/UserDetailInfoResponse.java
@@ -6,22 +6,28 @@ import org.hae.sfaas.domain.user.model.UserRole;
 import java.time.LocalDateTime;
 
 public record UserDetailInfoResponse(
+        Long userId,
         Long factoryId,
         String userEmail,
         String userName,
         Long userMgr,
         UserRole userRole,
+        String department,
+        String position,
         LocalDateTime lastLoginAt,
         String factoryCode,
         String factoryLocation
 ) {
     public static UserDetailInfoResponse of(DetailUser detailUser) {
         return new UserDetailInfoResponse(
+                detailUser.getUserId(),
                 detailUser.getFactoryId(),
                 detailUser.getUserEmail(),
                 detailUser.getUserName(),
                 detailUser.getUserMgr(),
                 detailUser.getUserRole(),
+                detailUser.getDepartment(),
+                detailUser.getPosition(),
                 detailUser.getLastLoginAt(),
                 detailUser.getFactoryCode(),
                 detailUser.getFactoryLocation()

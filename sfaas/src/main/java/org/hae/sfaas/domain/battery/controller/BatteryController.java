2bd18ad470250d8f9551a848df84f8a42b0171b6
Choi YoungLyn <joeuni0129@gmail.com>
2024-12-16 17:14:28 +0900

    [Feat] #14 - Battery 총 양품/불량품 조회 API 구현

---
+++ sfaas/src/main/java/org/hae/sfaas/domain/battery/controller/BatteryController.java
@@ -0,0 +1,26 @@
+package org.hae.sfaas.domain.battery.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.hae.sfaas.domain.battery.service.BatteryService;
+import org.hae.sfaas.global.common.response.SFaaSResponse;
+import org.hae.sfaas.global.common.response.SuccessType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BatteryController {
+
+    private final BatteryService batteryService;
+
+    @GetMapping("/battery/status")
+    public ResponseEntity<SFaaSResponse<?>> getVoltageInfos(@RequestHeader("userId") Long userId,
+                                                            @RequestParam(value = "factoryId", required = false) Long factoryId){
+        return ResponseEntity.status(HttpStatus.OK).body(SFaaSResponse.success(SuccessType.GET_BATTERYPACK_VOLTAGE_SUCCESS, batteryService.getBatteryStatusInfo(userId, factoryId)));
+    }
+
+}

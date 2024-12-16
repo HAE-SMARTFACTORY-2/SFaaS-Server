747c0b7f006a20e75fd97d69e174f047a0812012
Choi YoungLyn <joeuni0129@gmail.com>
2024-12-16 22:19:01 +0900

    [Add] #19 - 시간별 생산량 조회에 필요한 DTO 생성

---
+++ sfaas/src/main/java/org/hae/sfaas/domain/battery/dto/response/BatteryOutputResponse.java
@@ -0,0 +1,17 @@
+package org.hae.sfaas.domain.battery.dto.response;
+
+import org.hae.sfaas.domain.battery.model.BatteryOutput;
+
+public record BatteryOutputResponse(
+        Long factoryId,
+        int totalCount,
+        int targetCount
+) {
+    public static BatteryOutputResponse of(BatteryOutput batteryOutput){
+        return new BatteryOutputResponse(
+                batteryOutput.getFactoryId(),
+                batteryOutput.getTotalCount(),
+                batteryOutput.getTotalCount()
+        );
+    }
+}

84c0a6de08bef0bd2ed983bcf076b41a5b775e96
Hwangwoosam <sam1783@naver.com>
2024-12-16 20:19:30 +0900

    [add]#20 Aging 공정 관련 MVC 추가

--- sfaas/src/main/java/org/hae/sfaas/domain/aging/mapper/AgingMapper.java
+++ sfaas/src/main/java/org/hae/sfaas/domain/aging/mapper/AgingMapper.java
@@ -1,7 +1,14 @@
 package org.hae.sfaas.domain.aging.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.hae.sfaas.domain.aging.model.AgingStatus;
+import org.hae.sfaas.domain.aging.model.AgingTempInfo;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Mapper
 public interface AgingMapper {
+    List<AgingTempInfo> findTempInfos(Long factoryId, LocalDate startAt,LocalDate endAt,String filter);
+    List<AgingStatus> findStatusCount(Long factoryId, LocalDate startAt,LocalDate endAt,String filter);
 }
